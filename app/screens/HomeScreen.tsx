72f79a000c284db3e0f9e03dc9fc291fc9e8856a
Jack Stork <jksd2d@umsystem.edu>
2023-06-06 17:06:17 -0500

    First day setup. Work still needed

---
+++ app/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment, useState } from 'react';
+import { View, Text } from 'react-native';
+
+function HomeScreen({navigation}: {navigation: any}) {
+    return (
+        <Fragment>
+            <View></View>
+        </Fragment>
+    );
+}
+
+export default HomeScreen;
